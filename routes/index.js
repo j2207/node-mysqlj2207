5094fcbff11c09b37720c635bff62c297be12939
j2207 <j2207@matsue-ct.ac.jp>
2025-07-03 04:56:42 +0000

    11

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'todo_app'
-});
-
-// サーバ起動時に一度だけ接続
-connection.connect((err) => {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('MySQL connected as id ' + connection.threadId);
-});
+let todos = [];
 
 router.get('/', function (req, res, next) {
-  connection.query(
-    `select * from tasks;`,
-    (error, results) => {
-      if (error) {
-        console.log(error);
-        return res.status(500).send('DB error');
-      }
-      res.render('index', {
-        title: 'ToDo App',
-        todos: results,
-      });
-    }
-  );
+  res.render('index', {
+    title: 'ToDo App',
+    todos: todos,
+  });
 });
 
 router.post('/', function (req, res, next) {
   const todo = req.body.add;
-  connection.query(
-    `insert into tasks (user_id, content) values (1, ?)`,
-    [todo],
-    (error, results) => {
-      if (error) {
-        console.log(error);
-        return res.status(500).send('DB error');
-      }
-      res.redirect('/');
-    }
-  );
+  todos.push(todo);
+  res.redirect('/');
 });
 
 module.exports = router;
