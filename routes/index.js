ee057a79613b600eecfa9e713a96d1abf8577537
j2207 <j2207@matsue-ct.ac.jp>
2025-08-07 02:32:10 +0000

    15

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'todo_app'
-});
 
-let todos = [];
 router.get('/', function (req, res, next) {
   knex("tasks")
     .select("*")
@@ -27,6 +19,7 @@ router.get('/', function (req, res, next) {
       });
     });
 });
+
 router.post('/', function (req, res, next) {
   const todo = req.body.add;
   knex("tasks")
@@ -41,5 +34,8 @@ router.post('/', function (req, res, next) {
       });
     });
 });
+
 router.use('/signup', require('./signup'));
+router.use('/signin', require('./signin'));
+
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,88 +1,88 @@
-// const express = require('express');
-// const router = express.Router();
-// const knex = require("../db/knex");
-// router.get('/', function (req, res, next) {
-//   res.render('signup', {
-//     title: 'Sign up',
-//   });
-// });
-
-// router.post('/', function (req, res, next) {
-//   const username = req.body.username;
-//   const password = req.body.password;
-//   const repassword = req.body.repassword;
-
-//   knex("users")
-//     .where({name: username})
-//     .select("*")
-//     .then(function (result) {
-//       if (result.length !== 0) {
-//         res.render("signup", {
-//           title: "Sign up",
-//           errorMessage: ["このユーザ名は既に使われています"],
-//         }) 
-//       } else if (password === repassword) {
-//         knex("users")
-//           .insert({name: username, password: password})
-//           .then(function () {
-//             res.redirect("/");
-//           })
-//           .catch(function (err) {
-//             console.error(err);
-//             res.render("signup", {
-//               title: "Sign up",
-//               errorMessage: [err.sqlMessage],
-//             });
-//           });
-//       } else {
-//         res.render("signup", {
-//           title: "Sign up",
-//           errorMessage: ["パスワードが一致しません"],
-//         });
-//       }
-//     })
-//     .catch(function (err) {
-//       console.error(err);
-//       res.render("signup", {
-//         title: "Sign up",
-//         errorMessage: [err.sqlMessage],
-//       });
-//     });
-// });
-
-// module.exports = router;
-
-
-
 const express = require('express');
 const router = express.Router();
-const knex = require('../db/knex');
-
-router.get('/', (req, res) => {
-  res.render('signup', { title: 'Sign up' });
+const knex = require("../db/knex");
+router.get('/', function (req, res, next) {
+  res.render('signup', {
+    title: 'Sign up',
+  });
 });
 
-router.post('/', async (req, res) => {
-  const { username, password, repassword } = req.body;
-  if (!username || !password || !repassword) {
-    return res.render('signup', { title: 'Sign up', error: '全て入力してください' });
-  }
-  if (password !== repassword) {
-    return res.render('signup', { title: 'Sign up', error: 'パスワードが一致しません' });
-  }
-  try {
-    // 既存ユーザー確認
-    const users = await knex('users').where({ username });
-    if (users.length > 0) {
-      return res.render('signup', { title: 'Sign up', error: 'そのユーザー名は既に使われています' });
-    }
-    // ユーザー登録
-    await knex('users').insert({ username, password });
-    res.redirect('/');
-  } catch (err) {
-    console.error(err);
-    res.render('signup', { title: 'Sign up', error: '登録に失敗しました' });
-  }
+router.post('/', function (req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+  const repassword = req.body.repassword;
+
+  knex("users")
+    .where({name: username})
+    .select("*")
+    .then(function (result) {
+      if (result.length !== 0) {
+        res.render("signup", {
+          title: "Sign up",
+          errorMessage: ["このユーザ名は既に使われています"],
+        }) 
+      } else if (password === repassword) {
+        knex("users")
+          .insert({name: username, password: password})
+          .then(function () {
+            res.redirect("/");
+          })
+          .catch(function (err) {
+            console.error(err);
+            res.render("signup", {
+              title: "Sign up",
+              errorMessage: [err.sqlMessage],
+            });
+          });
+      } else {
+        res.render("signup", {
+          title: "Sign up",
+          errorMessage: ["パスワードが一致しません"],
+        });
+      }
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render("signup", {
+        title: "Sign up",
+        errorMessage: [err.sqlMessage],
+      });
+    });
 });
 
 module.exports = router;
+
+
+
+// const express = require('express');
+// const router = express.Router();
+// const knex = require('../db/knex');
+
+// router.get('/', (req, res) => {
+//   res.render('signup', { title: 'Sign up' });
+// });
+
+// router.post('/', async (req, res) => {
+//   const { username, password, repassword } = req.body;
+//   if (!username || !password || !repassword) {
+//     return res.render('signup', { title: 'Sign up', error: '全て入力してください' });
+//   }
+//   if (password !== repassword) {
+//     return res.render('signup', { title: 'Sign up', error: 'パスワードが一致しません' });
+//   }
+//   try {
+//     // 既存ユーザー確認
+//     const users = await knex('users').where({ username });
+//     if (users.length > 0) {
+//       return res.render('signup', { title: 'Sign up', error: 'そのユーザー名は既に使われています' });
+//     }
+//     // ユーザー登録
+//     await knex('users').insert({ username, password });
+//     res.redirect('/');
+//   } catch (err) {
+//     console.error(err);
+//     res.render('signup', { title: 'Sign up', error: '登録に失敗しました' });
+//   }
+// });
+
+// module.exports = router;
