faf15c94e07bcb863033f0c464ba3352d796020d
j2207 <j2207@matsue-ct.ac.jp>
2025-08-07 05:02:27 +0000

    15

--- routes/index.js
+++ routes/index.js
@@ -39,4 +39,5 @@ router.post('/', function (req, res, next) {
 });
 router.use('/signup', require('./signup'));
 router.use('/signin', require('./signin'));
+router.use('/logout', require('./logout'));
 module.exports = router;
