105cfea91a1a11aa4025e2015fdb76a843024bb0
j2207 <j2207@matsue-ct.ac.jp>
2025-08-07 13:03:54 +0000

    19

--- routes/signup.js
+++ routes/signup.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const knex = require('../db/knex');
+const knex = require("../db/knex");
 const bcrypt = require("bcrypt");
- 
+
 router.get('/', function (req, res, next) {
-  const userId = req.session.userid;
-const isAuth = Boolean(userId);
+  const isAuth = req.isAuthenticated();
   res.render('signup', {
     title: 'Sign up',
-    isAuth: isAuth
+    isAuth: isAuth,
   });
 });
- 
+
 router.post('/', function (req, res, next) {
-  const userId = req.session.userid;
-  const isAuth = Boolean(userId);
+  const isAuth = req.isAuthenticated();
   const username = req.body.username;
   const password = req.body.password;
   const repassword = req.body.repassword;
- 
+
   knex("users")
     .where({name: username})
     .select("*")
@@ -26,14 +24,13 @@ router.post('/', function (req, res, next) {
       if (result.length !== 0) {
         res.render("signup", {
           title: "Sign up",
-          isAuth: isAuth,
           errorMessage: ["このユーザ名は既に使われています"],
+          isAuth: isAuth,
         })
       } else if (password === repassword) {
         const hashedPassword = await bcrypt.hash(password, 10);
-        //console.log(hashedPassword);
         knex("users")
-          .insert({name: username, password: password})
+          .insert({name: username, password: hashedPassword})
           .then(function () {
             res.redirect("/");
           })
@@ -41,15 +38,15 @@ router.post('/', function (req, res, next) {
             console.error(err);
             res.render("signup", {
               title: "Sign up",
-              isAuth: isAuth,
               errorMessage: [err.sqlMessage],
+              isAuth: isAuth,
             });
           });
       } else {
         res.render("signup", {
           title: "Sign up",
-          isAuth: isAuth,
           errorMessage: ["パスワードが一致しません"],
+          isAuth: isAuth,
         });
       }
     })
@@ -57,9 +54,10 @@ router.post('/', function (req, res, next) {
       console.error(err);
       res.render("signup", {
         title: "Sign up",
-        isAuth: isAuth,
         errorMessage: [err.sqlMessage],
+        isAuth: isAuth,
       });
     });
 });
+
 module.exports = router;
