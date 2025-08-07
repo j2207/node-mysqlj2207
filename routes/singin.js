2865d907666173a2899fb3a112cfed97206900b8
j2207 <j2207@matsue-ct.ac.jp>
2025-08-07 02:35:36 +0000

    15

--- routes/singin.js
+++ routes/singin.js
@@ -7,4 +7,34 @@ router.get('/', function (req, res, next) {
   });
 });
 
+router.post('/', function (req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  knex("users")
+    .where({
+      name: username,
+      password: password,
+    })
+    .select("*")
+    .then((results) => {
+      if (results.length === 0) {
+        res.render("signin", {
+          title: "Sign in",
+          errorMessage: ["ユーザが見つかりません"],
+        });
+      } else {
+        res.redirect('/');
+      }
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.render("signin", {
+        title: "Sign in",
+        errorMessage: [err.sqlMessage],
+        isAuth: false,
+      });
+    });
+});
+
 module.exports = router;
